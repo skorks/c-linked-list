7eb9b82ff1c4733b73de30cb9c8a574659f60390
Rishit Chaudhary <rishit.c@live.com>
2020-06-16 01:18:12 +0530

    Improved the delete function.

--- linkedlist.c
+++ linkedlist.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "linkedlist_struct.h"
 #include "linkedlist.h"
 
-struct node {
-  int data;
-  struct node * next;
-};
-
-struct list {
-  Node * head; 
-};
-
-Node * createnode(int data);
 
 Node * createnode(int data){
   Node * newNode = malloc(sizeof(Node));
@@ -32,22 +24,32 @@ List * makelist(){
   return list;
 }
 
+/**
+ * This function displays the elements of the linked list in order from head to tail.
+ * Example: If a linked list contains the elements 1,2,3 in this order with 1 at the head then
+ * the display function will display the linked list as: 1 -> 2 -> 3 ->
+ * 
+ * The -> after 3 points to nothing to indicate that it is pointing to NULL
+ * 
+ * The function does not print anything in case the linked list is empty.
+ * */
 void display(List * list) {
-  Node * current = list->head;
-  if(list->head == NULL) 
-    return;
-  
-  for(; current != NULL; current = current->next) {
-    printf("%d\n", current->data);
+  for(Node * current = list->head; current != NULL; current = current->next) {
+    printf("%d -> ", current->data);
   }
 }
 
+
+/**
+ * This functions adds an element at the end of the linked list.
+ * */ 
 void add(int data, List * list){
-  Node * current = NULL;
+  // If the linked list is empty
   if(list->head == NULL){
     list->head = createnode(data);
   }
-  else {
+  else { // If there are elements present in the linked list
+    Node * current = NULL;
     current = list->head; 
     while (current->next!=NULL){
       current = current->next;
@@ -56,21 +58,49 @@ void add(int data, List * list){
   }
 }
 
+
+// void delete(int data, List * list){
+//   Node * current = list->head;            
+//   Node * previous = current;           
+//   while(current != NULL){           
+//     if(current->data == data){      
+//       previous->next = current->next;
+//       if(current == list->head)
+//         list->head = current->next;
+//       free(current);
+//       return;
+//     }                               
+//     previous = current;             
+//     current = current->next;        
+//   }                                 
+// }  
+
+
+/**
+ * This function deletes the first occurrence of the node containing the data value in it.
+ * NOTE: This function only uses a single pointer for the deletion.
+ * */
 void delete(int data, List * list){
-  Node * current = list->head;            
-  Node * previous = current;           
-  while(current != NULL){           
-    if(current->data == data){      
-      previous->next = current->next;
-      if(current == list->head)
-        list->head = current->next;
-      free(current);
-      return;
-    }                               
-    previous = current;             
-    current = current->next;        
-  }                                 
-}                                   
+  Node * finder = list->head;  // Finder will be used to find the first occurrence of the node containing the value data
+
+  while(finder != NULL){
+    if(finder->data == data){
+      Node * temp = finder->next;  // temp points to the next node
+
+      if(temp != NULL){ // If the node to delete is the first node or somewhere in-between in the linked list
+        finder->data = temp->data;  // Copying the data from the next node to the current node
+        finder->next = temp->next;  // Setting the next of the current node to the next value of the next node
+        free(temp);
+        temp = NULL;  // To avoid dangling pointers
+      } else { // If the node to delete is the last node in the linked list
+        free(finder);
+        finder = NULL; // To avoid dangling pointers
+      }
+    }
+    finder = finder->next;
+  }
+}
+
 
 void reverse(List * list){
   Node * reversed = NULL;
@@ -84,6 +114,7 @@ void reverse(List * list){
   }
   list->head = reversed;
 }
+
 
 void destroy(List * list){
   Node * current = list->head;
